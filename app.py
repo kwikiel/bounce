9633a2ad3975a73cee3cd224bde5dec53517b3a2
Kacper Wikiel <random8902@gmail.com>
2015-07-23 17:23:50 +0200

    Fwp

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask import render_template, url_for
 from flask.ext.split import split, finished
 app = Flask(__name__)
 app.register_blueprint(split)
+app.config['REDIS_URL'] = 'redis://172.17.0.5:32768'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 app.config['SPLIT_DB_FAILOVER'] = True
 
