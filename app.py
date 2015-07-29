4110c4570d25aa7115438d4e4b1fa22c357caf3a
Kacper Wikieł <kwikiel@users.noreply.github.com>
2015-07-30 01:25:53 +0200

    Env variables
    https://github.com/ohardy/dokku-redis

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import render_template, url_for
 from flask.ext.split import split, finished
 app = Flask(__name__)
 app.register_blueprint(split)
-app.config['REDIS_URL'] =  'redis://172.17.0.13:6379'
-app.config['REDIS_IP'] = '172.17.0.13'
-app.config['REDIS_PORT'] = '6379'
+app.config['REDIS_URL'] =  'redis://172.17.0.53:32770'
+app.config['REDIS_IP'] = '172.17.0.53'
+app.config['REDIS_PORT'] = '32770'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 app.config['SPLIT_DB_FAILOVER'] = True
 
