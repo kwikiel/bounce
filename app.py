b2f034def9d6834b2d7070fbd425040c1ee65595
Kacper Wikiel <random8902@gmail.com>
2015-11-14 20:57:46 +0100

    Hardcoded envs

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import render_template, url_for
 from flask.ext.split import split, finished
 app = Flask(__name__)
 app.register_blueprint(split)
-app.config['REDIS_URL'] =  'redis://172.17.0.53:32770'
-app.config['REDIS_IP'] = '172.17.0.53'
-app.config['REDIS_PORT'] = '32770'
+app.config['REDIS_URL'] =  'redis://dokku-redis-bounce:6379/0'
+#app.config['REDIS_IP'] = '172.17.0.53'
+#app.config['REDIS_PORT'] = '32770'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 app.config['SPLIT_DB_FAILOVER'] = True
 
