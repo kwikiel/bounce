8e563b316c8f500e47801c69a9d8c6df12188004
Kacper Wikiel <random8902@gmail.com>
2015-12-04 19:46:56 +0100

    PEP8 and redis hardcoded url

--- app.py
+++ app.py
@@ -1,22 +1,27 @@
 from flask import Flask
-from flask import render_template, url_for
+from flask import render_template
 from flask.ext.split import split, finished
+
+
 app = Flask(__name__)
 app.register_blueprint(split)
-app.config['REDIS_URL'] =  'redis://dokku-redis-bounce:6379/0'
-#app.config['REDIS_IP'] = '172.17.0.53'
-#app.config['REDIS_PORT'] = '32770'
+app.config['REDIS_URL'] = 'redis://dokku-redis-earn:6379/0'
+# app.config['REDIS_IP'] = '172.17.0.53'
+# app.config['REDIS_PORT'] = '32770'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 app.config['SPLIT_DB_FAILOVER'] = True
 
+
 @app.route('/')
 def landing():
-    return render_template("landing.html", options = ["A", "B"])
+    return render_template("landing.html", options=["A", "B"])
+
 
 @app.route('/what')
 def final():
     finished('cta')
     return "Thank for helping! Go back to work on your startup!"
+
 
 @app.route('/nope')
 def final2():
