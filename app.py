34f4e6baff46c18a0a4050ec2f8c247d030df45e
Kacper Wikiel <random8902@gmail.com>
2016-01-20 00:56:26 +0100

    RTFM http://flask-split.readthedocs.org/en/latest/

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ import sys
 import logging
 
 app = Flask(__name__)
-redis_url = os.getenv('REDISTOGO_URL', 'redis://localhost:6379')
+app.secret_key = os.urandom(64)
+redis_url = os.getenv('REDIS_URL', 'redis://localhost:6379')
 app.config['REDIS_URL'] = redis_url
 app.register_blueprint(split)
-app.secret_key = os.urandom(64)
 app.config['SPLIT_DB_FAILOVER'] = True
 
 # Debug
