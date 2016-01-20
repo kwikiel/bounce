42337721880aa4c7203a0016c3bc87419fa849af
Kacper Wikiel <random8902@gmail.com>
2016-01-21 00:44:32 +0100

    Model database

---
+++ models.py
@@ -0,0 +1,14 @@
+from app import db
+
+
+class Alternative(db.Model):
+    __tablename___ = 'alternatives'
+    id = db.Column(db.Integer, primary_key=True)
+    text = db.Column(db.String(500), unique=True)
+
+    def __init__(self, id, text):
+        self.id = id
+        self.text = text
+
+    def __repr__(self):
+        return '<Alternative: %r>' % self.text
